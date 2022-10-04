bf2eb20b7a28f233a672a1a641eea2b110291cc5
TatyanaZakharova <57043523+Tatyana210@users.noreply.github.com>
2022-10-04 18:56:38 +0300

    Add files via upload

---
+++ Ex2/Ex2/Ex2.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите имя");
+string name = Console.ReadLine();
+if (name == "Вячеслав")
+    Console.WriteLine(" Привет, Вячеслав");
+else
+    Console.WriteLine("Нет такого имени");
