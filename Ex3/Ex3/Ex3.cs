5b0d157bf7f157b65d214ee376aa907bdfb4910f
TatyanaZakharova <57043523+Tatyana210@users.noreply.github.com>
2022-10-04 18:57:22 +0300

    Add files via upload

---
+++ Ex3/Ex3/Ex3.cs
@@ -0,0 +1,8 @@
+﻿
+    int[] array = new int[] { 1, 3, 99, 125, 748, 66 };
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 3 == 0)
+            Console.WriteLine(array[i]);
+    }
+    
